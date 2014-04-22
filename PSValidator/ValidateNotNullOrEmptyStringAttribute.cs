06164f6bf8cc2f28216af67ed0a8a85e24dbed87
aetos <aetos@aerie.jp>
2014-04-22 10:48:53 +0900

    ValidateNotNullOrEmptyStringAttribute

---
+++ PSValidator/ValidateNotNullOrEmptyStringAttribute.cs
@@ -0,0 +1,31 @@
+﻿namespace PSValidator
+{
+    using System;
+    using System.Linq;
+    using System.Management.Automation;
+
+    using PSValidator.Properties;
+
+    public class ValidateNotNullOrEmptyStringAttribute :
+        ValidateEnumeratedArgumentsAttribute
+    {
+        protected override void ValidateElement(object element)
+        {
+            if (element == null)
+            {
+                throw new ValidationMetadataException(Resources.ArgumentIsNull);
+            }
+
+            string value = element as string;
+            if (value == null)
+            {
+                throw new ValidationMetadataException(Resources.ArgumentIsNotString);
+            }
+
+            if (value.Length == 0)
+            {
+                throw new ValidationMetadataException(Resources.ArgumentIsEmptyString);
+            }
+        }
+    }
+}
