833b7fcd4509c6943b57b1fed1e0eae564804885
aetos <aetos@aerie.jp>
2014-04-22 10:48:46 +0900

    ValidateNotNullOrEmptyStringAttribute

---
+++ PSValidator/PathType.cs
@@ -0,0 +1,15 @@
+﻿namespace PSValidator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public enum PathType
+    {
+        Any,
+        Container,
+        Leaf
+    }
+}
---
+++ PSValidator/ValidatePathAttribute.cs
@@ -0,0 +1,41 @@
+﻿namespace PSValidator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Management.Automation;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ValidatePathAttribute :
+        ValidateArgumentsAttribute
+    {
+        private readonly string path;
+
+        public ValidatePathAttribute(string path)
+        {
+            this.path = path;
+
+            this.PathType = PathType.Any;
+            this.LiteralPath = false;
+        }
+
+        public string Path
+        {
+            get
+            {
+                return this.path;
+            }
+        }
+
+        public PathType PathType { get; set; }
+
+        public bool LiteralPath { get; set; }
+
+        protected override void Validate(
+            object arguments,
+            EngineIntrinsics engineIntrinsics)
+        {
+        }
+    }
+}
