59c9f408f42b7f62c30e795a0aebc82223249574
aetos <aetos@aerie.jp>
2014-04-23 02:13:58 +0900

    不要な using を除去

--- PSValidator/PathType.cs
+++ PSValidator/PathType.cs
@@ -1,10 +1,6 @@
 ﻿namespace PSValidator
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public enum PathType
     {
